3769b3adadf4170f4af1f65107e2e50983e3001d
Eli Chandler <eli.chandler@gmail.com>
2020-03-05 20:25:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#Get recipes name and check its not blank
+
+#Not blank function goes here
+
+#Main routine goes here
+recipe_name = input("Please enter the name of your recipe: ")
+print(recipe_name)
