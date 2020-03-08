7763ded8c1f55615771b6f2e091b7793bfb6337a
FloowDot <eli.chandler@gmail.com>
2020-03-08 21:26:45 +0000

    Added not blank function

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 #Get recipes name and check its not blank
 
 #Not blank function goes here
+def not_blank(question):
+
+  valid = False
+  while not valid:
+    response = input(question)
+    if response == "":
+      continue
+    else:
+      return response
+
+    return response
 
 #Main routine goes here
-recipe_name = input("Please enter the name of your recipe: ")
-print(recipe_name)
+recipe_name = not_blank("Please enter the name of your recipe: ")
+
+print("You are making {}".format(recipe_name))
